0e0c4e1ac808558703a3461dee8d6968b3922a2c
eidza <eidzaben24@gmail.com>
2023-04-17 21:38:27 -0500

    Backend of the Registration Done

--- app/src/main/java/com/example/goalkeeper/RegistrationActivity.java
+++ app/src/main/java/com/example/goalkeeper/RegistrationActivity.java
@@ -1,14 +1,92 @@
 package com.example.goalkeeper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
-
+    EditText editTextEmail, editTextPassword, editTextConfirmation;
+    ProgressBar progressBar;
+    MaterialButton buttonReg;
+    FirebaseAuth mAuth;
+    TextView textViewGoLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextConfirmation = findViewById(R.id.password_confirmation);
+        buttonReg = findViewById(R.id.signup_btn);
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progress_bar);
+        textViewGoLogin = findViewById(R.id.go_login);
+
+        textViewGoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password, confirmation;
+                email = String.valueOf(editTextEmail.getText().toString());
+                password = String.valueOf(editTextPassword.getText().toString());
+                confirmation = String.valueOf(editTextConfirmation.getText().toString());
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(RegistrationActivity.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(email)){
+                    Toast.makeText(RegistrationActivity.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (!password.equals(confirmation)){
+                    Toast.makeText(RegistrationActivity.this, "Passwords Must Match", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(RegistrationActivity.this, "Authentication Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(RegistrationActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
