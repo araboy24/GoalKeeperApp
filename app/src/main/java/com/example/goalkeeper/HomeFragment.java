2c90dc3b1923067ee16416e1f8ff889c25ecdf7c
eidza <eidzaben24@gmail.com>
2023-04-18 13:07:21 -0500

    Fixed things and started HomeFragment

---
+++ app/src/main/java/com/example/goalkeeper/HomeFragment.java
@@ -0,0 +1,17 @@
+package com.example.goalkeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeFragment  extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_log_out);
+
+    }
+}
+
