e28a89646c23ecfe949b03ad0b53cd06ac917f7b
Zain Chaudhry <zainchaudhry6565@gmail.com>
2023-04-24 05:32:08 -0500

    Added button to navigate back to final_home

--- app/src/main/java/com/example/goalkeeper/ProfileActivity.java
+++ app/src/main/java/com/example/goalkeeper/ProfileActivity.java
@@ -2,12 +2,74 @@ package com.example.goalkeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
 
 public class ProfileActivity extends AppCompatActivity {
+
+    Button go_back;
+    Button open_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Added by Zain
+        setContentView(R.layout.activity_profile);
+        getSupportActionBar().hide();
+        //setContentView(R.layout)
+
+
+        go_back  = findViewById(R.id.back_btn_profile);
+
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FinalHomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        open_settings = findViewById(R.id.edit_button);
+        open_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileSettings.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
+
+
+
+
 }
