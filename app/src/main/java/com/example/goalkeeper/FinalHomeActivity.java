1f49de218149ff619d244027e7ab49b590919b4c
Zain Chaudhry <zainchaudhry6565@gmail.com>
2023-04-24 05:33:37 -0500

    Added button object to access profile class

--- app/src/main/java/com/example/goalkeeper/FinalHomeActivity.java
+++ app/src/main/java/com/example/goalkeeper/FinalHomeActivity.java
@@ -31,6 +31,9 @@ import java.util.List;
 
 public class FinalHomeActivity extends AppCompatActivity {
     Button add_goal;
+
+    //Added by Zain
+    Button profile;
     private FirebaseFirestore db;
     FirebaseUser user;
     FirebaseAuth auth;
@@ -108,5 +111,18 @@ public class FinalHomeActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        //Added by Zain
+        profile = findViewById(R.id.profile_button);
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
