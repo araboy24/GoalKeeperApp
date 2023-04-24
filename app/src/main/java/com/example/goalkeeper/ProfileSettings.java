38074ba7a775666095ce5279304fa0eb1f746a4a
Zain Chaudhry <zainchaudhry6565@gmail.com>
2023-04-24 05:36:29 -0500

    Added profile settings activity in xml and configured save btn in settings ..needs modification to save edits tho

---
+++ app/src/main/java/com/example/goalkeeper/ProfileSettings.java
@@ -0,0 +1,62 @@
+package com.example.goalkeeper;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class ProfileSettings extends AppCompatActivity {
+    Button save;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Added by Zain
+        setContentView(R.layout.activity_profile_settings);
+        getSupportActionBar().hide();
+        //setContentView(R.layout)
+
+
+        save  = findViewById(R.id.save_button);
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+}
